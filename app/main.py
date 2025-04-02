f2a2cec99c6b0922918f45e3a9a7fdf17c4b58a2
ggrimace <leechseedsk@proton.me>
2025-04-02 07:04:31 -0400

    add create article route and form

--- app/main.py
+++ app/main.py
@@ -1,14 +1,52 @@
-from fastapi import FastAPI
-from app import models
-from app.database import engine
-from app.routers import users
-
-models.Base.metadata.create_all(bind=engine)
+from fastapi import FastAPI, Request, Depends
+from sqlalchemy.orm import Session
+from .database import SessionLocal
+from .models import Article
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-app.include_router(users.router)
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
-def read_root():
-    return {"message": "UnicursalDB app is alive"}
+def homepage(request: Request, db: Session = Depends(get_db)):
+    articles = db.query(Article).all()
+    return templates.TemplateResponse("home.html", {"request": request, "articles": articles})
+
+from fastapi import Form
+from fastapi.responses import RedirectResponse
+from app.models import Article, Revision
+
+@app.get("/articles/new")
+def new_article_form(request: Request):
+    return templates.TemplateResponse("new_article.html", {"request": request})
+
+@app.post("/articles/new")
+def create_article(
+    title: str = Form(...),
+    content: str = Form(...),
+    db: Session = Depends(get_db)
+):
+    article = Article(title=title, is_redirect=False, created_by=1)
+    db.add(article)
+    db.flush()  # Get article.id before creating revision
+
+    revision = Revision(
+        article_id=article.id,
+        content=content,
+        edited_by=1,
+        summary="Initial version"
+    )
+    db.add(revision)
+    db.commit()
+
+    return RedirectResponse("/", status_code=303)
